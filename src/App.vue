aad88649f9ec700c61e455fcacf0f7dd49b1a9b9
Langery <1245066197@qq.com>
2023-09-11 09:38:01 +0800

    merge

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,21 @@
   <main>
     <p>This is the {{ number }} Page</p>
     <p>The first number is {{showNum}}</p>
+    <p>The reactive name is {{person.name}}, and age is {{person.age}}</p>
+    <main-page></main-page>
   </main>
 </template>
 
 <script lang="ts">
-import { onMounted, onUnmounted, ref, watch, watchEffect } from 'vue'
+import { onMounted, onUnmounted, reactive, ref, watch, watchEffect } from 'vue'
+import MainPage from './component/MainPage.vue';
 
 // import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'First Vue3',
+  components: {
+    MainPage
+  },
   data(){
     return{
       nickname: 'Vue 3',
@@ -27,6 +33,10 @@ export default {
   },
   // Composition API Enter
   setup (){
+    let person = reactive({
+      name: 'react body',
+      age: 18
+    })
     let watchObj = ref({
       name: 'watch body',
       age: 20
@@ -44,6 +54,10 @@ export default {
     onUnmounted(() => {
 
     })
+    return {
+      person,
+      watchObj
+    }
   }
 }
 </script>
---
+++ src/component/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    This is main page.
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'main page',
+  data() {
+    return {
+
+    }
+  },
+  setup(){
+    return {}
+  }
+}
+</script>
